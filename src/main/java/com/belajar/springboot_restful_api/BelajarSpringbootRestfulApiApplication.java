96eb6c4096df01f76631b82961d4aab0d39a2256
purnama713 <komarudinpurnama@gmail.com>
2025-12-27 14:13:26 +0700

    Initial Commit

---
+++ src/main/java/com/belajar/springboot_restful_api/BelajarSpringbootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.springboot_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringbootRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringbootRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajar/springboot_restful_api/BelajarSpringbootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.springboot_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringbootRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
